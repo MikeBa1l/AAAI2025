41b0a5348343266cce7000ed9c2d3ed30ec3e2d9
MikeBa1l <sanmikepass@163.com>
2024-08-31 12:21:44 +0800

    Delete train.sh

--- train.sh
+++
@@ -1,3 +0,0 @@
-python main.py --dataset NELL-One --data_path ./Nell-One \
---few 1 --prefix nell1_re --learning_rate 0.001 \
---checkpoint_epoch 1000 --eval_epoch 1000 --batch_size 1024 --device 6 --step train

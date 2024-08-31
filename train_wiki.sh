f7184e9513ce1089ebb1782b7c3e1472977ac7bb
MikeBa1l <sanmikepass@163.com>
2024-08-31 12:22:01 +0800

    Delete train_wiki.sh

--- train_wiki.sh
+++
@@ -1,3 +0,0 @@
-python main.py --dataset Wiki-One --data_path ./Wiki-One \
---few 1 --prefix wiki1 --learning_rate 0.001 \
---checkpoint_epoch 1000 --eval_epoch 1000 --batch_size 1024 --device 6 --step train
